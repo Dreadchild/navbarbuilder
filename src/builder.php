a7757621a76926253b510047fe539d57f2d49c03
Pairing Station #1 <resellercore+pairing1@godaddy.com>
2014-03-26 15:29:20 -0700

    Test using a wrapper around curl to slurp HTML

--- src/builder.php
+++ src/builder.php
@@ -1,3 +1,17 @@
 <?php
+  require('nav_curl.php');
+  class Builder
+  {
+    private $curl;
+    function __construct($curl=false)
+    {
+      $this->curl = $curl ? $curl : new NavCurl();
+    }
 
+    public function slurp()
+    {
+      $this->curl->slurp();
+      return "hellow world";
+    }
+  }
 ?>
--- test/test_builder.php
+++ test/test_builder.php
@@ -4,11 +4,49 @@
 
   class BuilderTest extends PHPUnit_Framework_TestCase
   {
-    public function testThatTrueIsTrue()
+    public function setup()
     {
-      $this->assertEquals(true, true);
+      $this->builder = new Builder(new FakeNavCurl("hellow world"));
+    }
+
+    public function testSlurp()
+    {
+      $this->assertEquals($this->builder->slurp(), "hellow world");
+    }
+
+    public function testSlupWithCurl()
+    {
+      $spy = new CurlWrapperSpy();
+      $builder = new Builder($spy);
+      $builder->slurp();
+      $this->assertTrue($spy->called);
     }
   }
 
+  /**
+  *
+  */
+  class CurlWrapperSpy
+  {
+    public $called = false;
 
+    public function slurp()
+    {
+      $this->called = true;
+    }
+  }
+
+  class FakeNavCurl
+  {
+    private $slurp_return;
+
+    function __construct($slurp_return) {
+      $this->slurp_return = $slurp_return;
+    }
+
+    public function slurp()
+    {
+      return $this->slurp_return;
+    }
+  }
 ?>
