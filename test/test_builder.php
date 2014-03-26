1da5f452f821b9d4c2c672a67520d616715009b4
Pairing Station #1 <resellercore+pairing1@godaddy.com>
2014-03-26 15:09:39 -0700

    Working test environment

---
+++ test/test_builder.php
@@ -0,0 +1,14 @@
+<?php
+
+  require_once(dirname(dirname(__FILE__))."/src/builder.php");
+
+  class BuilderTest extends PHPUnit_Framework_TestCase
+  {
+    public function testThatTrueIsTrue()
+    {
+      $this->assertEquals(true, true);
+    }
+  }
+
+
+?>
